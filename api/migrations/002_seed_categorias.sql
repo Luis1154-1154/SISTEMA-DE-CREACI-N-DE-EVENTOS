8d7f8d195d90cfa90b7226274d66cfb89b58e70b
Luis1154-Luis1154 <lllamas5@ucol.mx>
2026-05-18 21:52:52 -0600

    chore(db): keep single database.sql, remove migrations and consolidated SQL

--- api/migrations/002_seed_categorias.sql
+++
@@ -1,8 +0,0 @@
--- Seed predefinidas para `categorias`
-INSERT INTO categorias (nombre) VALUES
-('Conferencias'),
-('Talleres'),
-('Deportes'),
-('Cultura'),
-('Sociales')
-ON DUPLICATE KEY UPDATE nombre = nombre;
